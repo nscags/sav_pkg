e2a919f052bb7b605a4a452acfe6a3e469f79f4a
aravindadusumalli <adusumalliaravind3@icloud.com>
2024-10-11 12:56:12 -0400

    Added additional Tests and 2 new graphs

--- sav_pkg/tests/engine_tests/engine_test_configs/__init__.py
+++ sav_pkg/tests/engine_tests/engine_test_configs/__init__.py
@@ -11,6 +11,24 @@ from .config_101 import config_101
 # Feasible-Path uRPF (config_200 - config_299)
 from .config_200 import config_200
 from .config_201 import config_201
+from .config_202 import config_202
+from .config_203 import config_203
+from .config_204 import config_204
+from .config_205 import config_205
+from .config_206 import config_206
+from .config_207 import config_207
+from .config_208 import config_208
+from .config_209 import config_209
+from .config_210 import config_210
+from .config_211 import config_211
+from .config_212 import config_212
+from .config_213 import config_213
+
+#config 2200- 2299: using graph 002
+from .config_2200 import config_2200
+
+#config 3200- 3299: using graph 003
+from .config_3200 import config_3200
 
 # EFP uRPF (config_300 - config_399)
 from .config_300 import config_300
@@ -29,13 +47,27 @@ engine_test_configs = [
     # config_004,
     # config_100,
     # config_101,
-    config_200,
-    config_201,
-    config_300,
-    config_301,
+    # config_200,
+    # config_201,
+    # config_202,
+    # config_203,
+    # config_204,
+    # config_205,
+    # config_206,
+    # config_207,
+    # config_208,
+    # config_209,
+    # config_210,
+    # config_211,
+    # config_212,
+    # config_213,
+    # config_2200,
+    config_3200,
+    # config_300,
+    # config_301,
     # config_302,
-    config_400,
-    config_401,
+    # config_400,
+    # config_401,
 ]
 
 __all__ = ["engine_test_configs"]
