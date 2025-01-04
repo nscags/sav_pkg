ae0a909cf224dc81f771b30be73ae72a41a062c1
nscags <njscaglione@gmail.com>
2025-01-04 18:37:54 -0500

    adding export2some policies

--- sav_pkg/simulation_engine/policies/bgp/bgp_export2some.py
+++ sav_pkg/simulation_engine/policies/bgp/bgp_export2some.py
@@ -1,12 +1,11 @@
-from bgpy.simulation_engine import BGP
-from bgpy.enums import Relationships 
-
+from bgpy.simulation_engine.policies.bgp import BGP
+from bgpy.shared.enums import Relationships
 
 class BGPExport2Some(BGP):
-    name: str = "BGP E2S"
+    name: str = "BGP Export2Some"
         
     def _propagate(
-        self: "BGP",
+        self: "BGPExport2Some",
         propagate_to: Relationships,
         send_rels: set[Relationships],
     ) -> None:
@@ -15,15 +14,25 @@ class BGPExport2Some(BGP):
         send_rels is the relationships that are acceptable to send
         """
 
+        # percent of providers to export to 
+        percent = 0.5 
+
         if propagate_to.value == Relationships.PROVIDERS.value:
             neighbors = self.as_.providers
 
-            for prefix, unprocessed_ann in self._local_rib.items():
+            num = max(1, int(len(neighbors) * percent))
+            some_neighbors = sorted(neighbors, key=lambda n: n.asn)[:num]
+
+            for _prefix, unprocessed_ann in self.local_rib.items():
                 if neighbors and unprocessed_ann.recv_relationship in send_rels:
                     ann = unprocessed_ann.copy({"next_hop_asn": self.as_.asn})
-            
-            for i, neighbor in enumerate(neighbors):
-                ann = ann.copy({"prefix": f"7.7.{7+i}.0/24"})
-                # print(f"\nBGPE2S: \nAnn: \n{ann} \n{neighbor}")
-                if not self._prev_sent(neighbor, ann):
-                    self._process_outgoing_ann(neighbor, ann, propagate_to, send_rels)
+                else:
+                    continue
+
+                for neighbor in some_neighbors:
+                    if ann.recv_relationship in send_rels and not self._prev_sent(
+                        neighbor, ann
+                    ):
+                        self._process_outgoing_ann(neighbor, ann, propagate_to, send_rels)
+        else:
+            super()._propagate(propagate_to, send_rels)
