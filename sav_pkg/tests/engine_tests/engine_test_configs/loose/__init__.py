9f49755c727194ea86ab8359aaa30615f2f8aece
User1 <40shaked@gmail.com>
2025-06-28 07:34:43 +0300

    added tests and fixes

--- sav_pkg/tests/engine_tests/engine_test_configs/loose/__init__.py
+++ sav_pkg/tests/engine_tests/engine_test_configs/loose/__init__.py
@@ -1,10 +1,19 @@
-from .loose_000 import loose_000
-from .loose_001 import loose_001
-from .loose_002 import loose_002
-from .loose_003 import loose_003
+# from .loose_000 import loose_000
+# from .loose_001 import loose_001
+# from .loose_002 import loose_002
+# from .loose_003 import loose_003
+# from .loose_004 import loose_004
+# from .loose_005 import loose_005
+# from .loose_006 import loose_006
+from .loose_009 import loose_009
 loose_test_configs = [
-    loose_000,
-    loose_001,
-    loose_002,
-    loose_003,
+    # loose_000,
+    # loose_001,
+    # loose_002,
+    # loose_003,
+    # loose_004,
+    # loose_005,
+    # loose_006,
+    # loose_007,
+loose_009
 ]
