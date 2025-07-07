abfef433f6566be7f16225936f76f04e77556155
User1 <40shaked@gmail.com>
2025-07-07 08:28:08 +0300

    added tests and fixes

--- sav_pkg/tests/engine_tests/engine_test_configs/refined_alg_a/__init__.py
+++ sav_pkg/tests/engine_tests/engine_test_configs/refined_alg_a/__init__.py
@@ -1,8 +1,8 @@
 from .ref_alg_a_001 import ref_alg_a_001
 from .ref_alg_a_002 import ref_alg_a_002
 
-from .ref_alg_a_004 import ref_alg_a_004
+from .ref_alg_a_006 import ref_alg_a_006
 refined_alg_a_test_configs = [
 
-    ref_alg_a_004
+    ref_alg_a_006
 ]
