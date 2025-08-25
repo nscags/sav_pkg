8a7bf9b5681b806203f67ec57482b35d5b3def00
nscags <njscaglione@gmail.com>
2025-08-25 17:54:51 -0400

    modified scripts

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-#SBATCH -J e2a
-#SBATCH -p lo-core
-#SBATCH --time=72:00:00                             # Timeout after 72 hours
-#SBATCH -n 50                                       # Asking for 20 cores
+#SBATCH -J e2a                                      # Name of program
+#SBATCH -o log_e2a.out                              # Name of output file
+#SBATCH -p lo-core                                  # Partition (general, lo-core)
+#SBATCH --time=72:00:00                             # Timeout after 72 hours (lo-core), 12 hours (general)
+#SBATCH -n 40                                       # Asking for cores
 #SBATCH --mail-type=ALL                             # Event(s) that triggers email notification (BEGIN,END,FAIL,ALL)
 #SBATCH --mail-user=nicholas.scaglione@uconn.edu    # Destination email address
-#SBATCH --mem=64G                                   # Request 64G of RAM
-# #SBATCH --mem-per-cpu=16G
-#SBATCH -o log_e2a.out
+#SBATCH --mem=64G                                   # Request RAM
+# #SBATCH --mem-per-cpu=16G                         # Request RAM per cpu core, had OOM errors
 
 # Source Virtual Environment
 source ~/sav/venv/bin/activate
