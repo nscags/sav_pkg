7295fd0d9f5f2aea3de38007bdce3ed3f9f9ace8
User1 <40shaked@gmail.com>
2025-09-30 14:00:57 +0300

    added tests and fixes

--- sav_pkg/tests/engine_tests/engine_test_configs/efp_alg_a/__init__.py
+++ sav_pkg/tests/engine_tests/engine_test_configs/efp_alg_a/__init__.py
@@ -1,10 +1,10 @@
 from .alg_a_001 import algo_a_001
 from .alg_a_002 import algo_a_002
 from .alg_a_004 import algo_a_004
-
+from .alg_a_005 import algo_a_005
 
 efp_alg_a_test_configs = [
 
-# algo_a_004
+algo_a_005
 
 ]
