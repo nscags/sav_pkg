e4d517d88c817f886fdabad53ed8f38884ca0eef
nscags <njscaglione@gmail.com>
2025-09-30 19:42:09 -0400

    minor change

--- sav_pkg/policies/bgp/bgpfull_noexport2some.py
+++ sav_pkg/policies/bgp/bgpfull_noexport2some.py
@@ -2,5 +2,6 @@ from bgpy.simulation_engine import BGPFull
 
 from .bgp_noexport2some import BGPNoExport2Some 
 
+
 class BGPFullNoExport2Some(BGPFull, BGPNoExport2Some):
     name: str = "BGPFull No-E2S"
