54c3802b91877c7120b4c483a14bf1dd8edb22d0
Username <135693323+shak6@users.noreply.github.com>
2025-09-30 14:44:49 +0300

    added tests and fixes

--- sav_pkg/tests/engine_tests/engine_test_configs/efp_alg_a/__init__.py
+++ sav_pkg/tests/engine_tests/engine_test_configs/efp_alg_a/__init__.py
@@ -3,9 +3,11 @@ from .alg_a_002 import algo_a_002
 from .alg_a_004 import algo_a_004
 from .alg_a_005 import algo_a_005
 from .alg_a_006 import algo_a_006
+from .alg_a_007 import algo_a_007
 efp_alg_a_test_configs = [
 
-algo_a_005,algo_a_006
+
+algo_a_007
 
 
 ]
