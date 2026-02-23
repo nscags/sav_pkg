520e6a1106fa038cb3b3f4e6e0e175740e74c7de
nscags <njscaglione@gmail.com>
2026-02-22 20:15:49 -0500

    removing graph factory, broken and unused

--- sav_pkg/simulation_framework/__init__.py
+++ sav_pkg/simulation_framework/__init__.py
@@ -1,7 +1,5 @@
 # from .sav_as_graph_analyzer import SAVASGraphAnalyzer
 
-# # from .sav_graph_factory import SAVGraphFactory
-
 # from .scenarios import SAVScenario
 # from .scenarios import SAVScenarioConfig
 # from .scenarios import SAVScenarioDSR
@@ -10,7 +8,6 @@
 
 __all__ = [
     "SAVASGraphAnalyzer",
-    "SAVGraphFactory",
     "SAVScenario",
     "SAVScenarioConfig",
     "SAVScenarioDSR",
